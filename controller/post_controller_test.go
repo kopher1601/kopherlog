f20774bfc0b59239c8d7e4a9086d3327958f9ca3
kopher <kopher1601@gmail.com>
2025-03-06 00:03:54 +0900

    refactor : Remove Rollback

--- controller/post_controller_test.go
+++ controller/post_controller_test.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
 	"kopherlog/config"
-	db "kopherlog/db"
 	"kopherlog/domain"
 	"kopherlog/ent"
 	"kopherlog/repository"
@@ -24,8 +24,56 @@ func TestMain(m *testing.M) {
 	config.Initialize()
 	code := m.Run()
 	os.Exit(code)
-
 }
+
+func WithTxTest(ctx context.Context, client *ent.Client, fn func(tx *ent.Tx) error) {
+	tx, err := client.Tx(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer func() {
+		if v := recover(); v != nil {
+			tx.Rollback()
+			panic(v)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			err = fmt.Errorf("%w: rolling back transaction: %v", err, rerr)
+		}
+		fmt.Println(err)
+	}
+	if err := tx.Rollback(); err != nil {
+		fmt.Println("committing transaction: %w", err)
+	}
+}
+
+//func Test_PostController_Post_Save(t *testing.T) {
+//	WithTxTest(context.Background(), config.SetupDB(t), func(tx *ent.Tx) error {
+//		r := gin.Default()
+//
+//		postCreate := &domain.PostCreate{
+//			Title:   "吉祥寺マンション",
+//			Content: "吉祥寺マンション購入します。",
+//		}
+//		var buf bytes.Buffer
+//		_ = json.NewEncoder(&buf).Encode(postCreate)
+//
+//		resp := httptest.NewRecorder()
+//		req, _ := http.NewRequest(http.MethodPost, "/posts", &buf)
+//		req.Header.Set("Content-Type", "application/json")
+//		postRepository := repository.NewPostRepository(tx.Client(), tx)
+//		postService := service.NewPostService(postRepository)
+//		postController := NewPostController(postService)
+//		r.POST("/posts", postController.PostCreate)
+//		r.ServeHTTP(resp, req)
+//		posts, err := postRepository.FindAll()
+//		assert.NoError(t, err)
+//		assert.Equal(t, http.StatusCreated, resp.Code)
+//		assert.Equal(t, 1, len(posts))
+//		return err
+//	})
+//}
 
 func Test_PostController_Post_Save(t *testing.T) {
 	client := config.SetupDB(t)
@@ -41,21 +89,15 @@ func Test_PostController_Post_Save(t *testing.T) {
 	resp := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodPost, "/posts", &buf)
 	req.Header.Set("Content-Type", "application/json")
-
-	context := context.Background()
-	db.WithTx(context, client, func(tx *ent.Tx) error {
-		postRepository := repository.NewPostRepository(tx.Client())
-		postService := service.NewPostService(postRepository)
-		postController := NewPostController(postService)
-		r.POST("/posts", postController.PostCreate)
-		r.ServeHTTP(resp, req)
-		posts, err := postRepository.FindAll()
-		assert.NoError(t, err)
-		assert.Equal(t, http.StatusCreated, resp.Code)
-		assert.Equal(t, 1, len(posts))
-		return nil
-	})
-
+	postRepository := repository.NewPostRepository(client)
+	postService := service.NewPostService(postRepository)
+	postController := NewPostController(postService)
+	r.POST("/posts", postController.PostCreate)
+	r.ServeHTTP(resp, req)
+	posts, err := postRepository.FindAll()
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, resp.Code)
+	assert.Equal(t, 1, len(posts))
 }
 
 func Test_PostController_PostCreate_Title_Required(t *testing.T) {
--- db/ent.go
+++ db/ent.go
@@ -4,9 +4,14 @@ import (
 	"context"
 	"fmt"
 	"kopherlog/ent"
+	"os"
 )
 
-func WithTx(ctx context.Context, client *ent.Client, fn func(tx *ent.Tx) error) error {
+func WithTx(ctx context.Context, client *ent.Client, testTx *ent.Tx, fn func(tx *ent.Tx) error) error {
+	if os.Getenv("ENV") == "TEST" {
+		fn(testTx)
+		return testTx.Rollback()
+	}
 	tx, err := client.Tx(ctx)
 	if err != nil {
 		return err
@@ -17,13 +22,19 @@ func WithTx(ctx context.Context, client *ent.Client, fn func(tx *ent.Tx) error) 
 			panic(v)
 		}
 	}()
+
 	if err := fn(tx); err != nil {
 		if rerr := tx.Rollback(); rerr != nil {
 			err = fmt.Errorf("%w: rolling back transaction: %v", err, rerr)
 		}
 		return err
 	}
-	if err := tx.Rollback(); err != nil {
+
+	//if os.Getenv("ENV") == "TEST" {
+	//	return tx.Rollback()
+	//}
+
+	if err := tx.Commit(); err != nil {
 		return fmt.Errorf("committing transaction: %w", err)
 	}
 	return nil
